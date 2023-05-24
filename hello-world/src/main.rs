2094efde82e7998d7fd232386812e39adbd91cb2
lcscosta <lcscosta@disroot.org>
2023-05-23 21:16:07 -0300

    Added an Hello World example

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Length, Settings};
+use iced::Sandbox;
+use iced::widget::{Container, Column, Text};
+
+struct HelloWorld;
+
+impl Sandbox for HelloWorld {
+    type Message = ();
+
+    fn new() -> Self {
+        HelloWorld
+    }
+
+    fn title(&self) -> String {
+        String::from("Hello, world!")
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+    }
+
+    fn view(&self) -> iced::Element<Self::Message> {
+        let label = Text::new("Hello, world!");
+        let column = Column::new().push(label);
+        Container::new(column).width(Length::Fill).center_x().height(Length::Fill).center_y().into()
+    }
+
+}
+
+fn main() -> iced::Result {
+    HelloWorld::run(Settings::default())
 }
